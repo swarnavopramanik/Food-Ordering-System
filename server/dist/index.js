cb52069d05e522fe08915bce859f9c51e7ef7c04
swarnavopramanik <105142693+swarnavopramanik@users.noreply.github.com>
2022-09-26 23:16:28 +0530

    completed setup of server and added babel to it, made mongo connection.

---
+++ server/dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv.default.config();
+
+const zomato = (0, _express.default)();
+zomato.use(_express.default.json());
+zomato.get("/", (req, res) => {
+  res.json({
+    massege: "Server is running"
+  });
+});
+const PORT = 4000;
+zomato.listen(PORT, () => {
+  console.log("Server is is running !!!");
+});
