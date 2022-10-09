2cb438829f077f9b745125e7e97b12dc8751f464
swarnavopramanik <105142693+swarnavopramanik@users.noreply.github.com>
2022-10-09 13:01:24 +0530

    completed server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import session from "express-session";
 
 // Private route authorization config
 import privateRouteConfig from "./config/route.config";
+import googleAuthConfig from "./config/google.config";
+
 
 // Database connection
 import ConnectDB from "./database/conection";
@@ -21,6 +23,8 @@ import Image from "./api/image";
 dotenv.config();
 
 privateRouteConfig(passport);
+googleAuthConfig(passport)
+
 
 const zomato = express();
 
